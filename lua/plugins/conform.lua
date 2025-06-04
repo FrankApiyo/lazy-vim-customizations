b226f417bc37650c5464cdf92fa329b6f1252a3d
FrankApiyo <franklineapiyo@gmail.com>
2025-06-04 10:47:17 +0300

    feat: Ensure prettier adds 4 spaces in json files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
       html = { "prettier" },
       typescript = { "prettier" },
       javascript = { "prettier" },
+      json = { "prettier" },
       svelte = { "prettier" },
       go = { "goimports", "gofmt" },
       clojure = { "zprint" },
@@ -14,6 +15,9 @@ return {
         command = "sh",
         args = { "-c", "cat | zprint" },
         stdin = true,
+      },
+      prettier = {
+        prepend_args = { "--tab-width", "4", "--use-tabs", "false" },
       },
     },
     format_on_save = {
